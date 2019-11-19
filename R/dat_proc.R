3f6de9cf063084995b6aec83a4b6f9651e7411de
fawda123 <mbafs2012@gmail.com>
2019-11-19 12:51:23 -0500

    prelim respiration analyses done

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -2,11 +2,13 @@ library(tidyverse)
 library(readxl)
 library(here)
 library(janitor)
+library(lmerTest)
+library(psycho)
 
 # consistent treatment terminology names
 trts <- tibble(
-  shrtlab = c("7.7A0.2", "7.7A0.5", "7.7C", "8.0A0.2", "8.0C"),
-  lngslab = c("7.7 Fluctuating 0.2A", "7.7 Fluctuating 0.5A", "7.7 Constant", "8.0 Fluctuating 0.2A", "8.0 Constant")
+  shrtlab = c("7.7C", "7.7A0.2", "7.7A0.5", "8.0C", "8.0A0.2"),
+  lngslab = c( "7.7 Constant", "7.7 Fluctuating 0.2A", "7.7 Fluctuating 0.5A", "8.0 Constant", "8.0 Fluctuating 0.2A")
   )
 
 # weight data processing --------------------------------------------------
@@ -36,10 +38,7 @@ alllen <- read_excel(here('data/raw', 'preliminary_oyster_data.xlsx'),
                      na = 'NA') %>% 
   rename(
     id = individual_id
-  ) %>% 
-  mutate(
-    lendif = 
-  )
+  ) 
 
 
 # respiration data --------------------------------------------------------
@@ -48,10 +47,84 @@ alllen <- read_excel(here('data/raw', 'preliminary_oyster_data.xlsx'),
 allres <- read.csv(here::here("data/raw", "Respiration_oyster_alldata.csv"), header = T, stringsAsFactors = F) %>% 
   clean_names %>% 
   na.omit %>% 
-  select(week, treatment, jar, id = oyster_id, resp = respiration_rate_umol_hr_g, species) %>% 
+  select(week, trt = treatment, jar, id = oyster_id, resp = respiration_rate_umol_hr_g, species) %>% 
   mutate(
-    ph = gsub('(^[0-9])*\\s.*$', '\\1', treatment), 
-    amp = gsub('^[0-9]\\.[0-9]\\s', '', treatment)
+    ph = gsub('(^[0-9])*\\s.*$', '\\1', trt), 
+    ph = factor(ph),
+    amp = gsub('^[0-9]\\.[0-9]\\s', '', trt), 
+    amp = factor(amp),
+    trt = factor(trt, levels = trts$lngslab, labels = trts$shrtlab), 
+    jar = factor(jar)
   )
 
 save(allres, file = here::here('data', 'allres.RData'), compress = 'xz')
+
+# respiration models ------------------------------------------------------
+
+# fit separate mixed models by species, week, jar as random effect
+resmods <- allres %>%
+  group_by(week, species) %>% 
+  nest %>% 
+  mutate(
+    mixmod = map(data, function(x){
+      
+      lmerTest::lmer(resp ~ trt + (1|jar), data = x)
+      
+    }),
+    anomod = map(mixmod, anova), 
+    summod = map(mixmod, analyze),
+    plomod = pmap(list(week, species, mixmod), function(week, species, mixmod){
+      
+      # estimates
+      mnsval <- get_means(mixmod, 'trt')
+      cnsval <- get_contrasts(mixmod, 'trt') %>% 
+        mutate(
+          sig = ifelse(p < 0.05, 'sig', 'notsig'),
+          sig = factor(sig,levels = c('notsig', 'sig'), labels = c(' not significant', 'significant'))
+        )
+      
+      # sample size
+      n <- mixmod@frame %>% nrow
+      
+      # labels
+      subttl <- paste0('Respiration (umol/hr/g), week ', week, ', ', species, ' oyster')
+      captns <- paste0('Significance where CI does not include zero, alpha = 0.05, total n = ', n)
+      
+      # mean estimate plots
+
+      p1 <- ggplot(mnsval, aes(x = trt, y = Mean)) + 
+        geom_point(size = 2) + 
+        geom_errorbar(aes(ymin = CI_lower, ymax = CI_higher), colour = 'black', size = 1) + 
+        labs(x = NULL, y = 'Estimated means (+/- 95% CI)', title = 'Treatment estimates', subtitle = subttl) + 
+        theme_ipsum() + 
+        theme(
+          panel.grid.major.y = element_blank(),
+          panel.grid.minor.y = element_blank()
+        ) + 
+        coord_flip()
+      
+      # contrast plots
+      p2 <- ggplot(cnsval, aes(x = Contrast, y = Difference, colour = sig)) + 
+        geom_point(aes(colour = sig), size = 2) + 
+        geom_errorbar(aes(ymin = CI_lower, ymax = CI_higher), colour = 'black', size = 1) + 
+        labs(x = NULL, y = 'Estimated differences (+/- 95% CI)', title = 'Treatment differences', subtitle = subttl, caption = captns) +
+        theme_ipsum() + 
+        theme(
+          legend.title = element_blank(), 
+          legend.position = 'bottom', 
+          panel.grid.major.y = element_blank(),
+          panel.grid.minor.y = element_blank()
+        ) +
+        geom_hline(yintercept = 0, linetype = 'dotted', size = 1) + 
+        scale_colour_manual(drop = F, values = c('black', 'tomato1')) + 
+        coord_flip()
+      
+      p1 + p2 + plot_layout(ncol  = 2)   
+      
+      
+    })
+  )
+
+save(resmods, file = here::here('data', 'resmods.RData'), compress = 'xz')
+
+
