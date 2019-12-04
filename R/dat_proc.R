7cf9a0ac457b881c5f67c7c0c0533f43e0bcd2bd
fawda123 <mbafs2012@gmail.com>
2019-12-04 15:37:14 -0500

    adding additional length analysis rmd

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -15,8 +15,8 @@ trts <- tibble(
 
 # consistent response variable terminology names
 rsps <- tibble(
-  shrtlab = c("delt_length", "delt_width", "dissolution_score", "final_length", "final_width", "folds", "irreg", "length_cm", "resp", "shell_weight", "tissue_weight", "total", "whole_organism_weight", "width_cm"),
-  lngslab = c("Delta length (cm)", "Delta width (cm)", "Dissolution score (0-3)", "Final length (cm)", "Final width (cm)", "Number of folds", "Number of irregularities", "Length (cm)", "Respiration (umol/hr/g)", "Shell weight (g)", "Tissue weight (g)", "Total observations",  "Whole weight (g)", "Width (cm)")
+  shrtlab = c("delt_length", "delt_width", "dissolution_score", "final_length", "final_width", "folds", "irreg", "rate_length", "rate_width", "resp", "shell_weight", "tissue_weight", "total", "whole_organism_weight"),
+  lngslab = c("Delta length (cm)", "Delta width (cm)", "Dissolution score (0-3)", "Final length (cm)", "Final width (cm)", "Number of folds", "Number of irregularities", "Length rate (cm/d)", "Width rate (cm/d)", "Respiration (umol/hr/g)", "Shell weight (g)", "Tissue weight (g)", "Total observations",  "Whole weight (g)")
 )
 
 # data processing to tidy -------------------------------------------------
@@ -43,11 +43,19 @@ alllen <- read.csv(here::here('data/raw', 'length_data.csv'), stringsAsFactors =
   select(week, trt = treatment, jar, id = individual_id, species, everything()) %>% 
   mutate(
     delt_length = final_length - initial_length, 
-    delt_width = final_width - initial_width
+    delt_width = final_width - initial_width, 
+    rate_length = case_when(
+      week == 2 ~ delt_length / 14, 
+      week == 4 ~ delt_length / 28
+    ),
+    rate_width = case_when(
+      week == 2 ~ delt_width / 14, 
+      week == 4 ~ delt_width / 28
+    )
   ) %>% 
   gather('var', 'val', -week, -trt, -jar, -id, -species) %>% 
   filter(week != 6) %>% 
-  filter(!(week %in% 0 & var %in% c('delt_length', 'delt_width'))) %>% 
+  filter(!(week %in% 0 & var %in% c('delt_length', 'delt_width', 'rate_width', 'rate_length'))) %>% 
   filter(!var %in% c('initial_length', 'initial_width'))
 
 ##
@@ -305,4 +313,234 @@ wtmods <- allexp %>%
 
 save(wtmods, file = here::here('data', 'wtmods.RData'), compress = 'xz')
 
+# Separate length analyses ------------------------------------------------
+
+# consistent response variable terminology names
+rspslen <- tibble(
+  shrtlab = c("delt_length", "delt_width", "final_length", "final_width"),
+  lngslab = c("Delta length (cm)", "Delta width (cm)", "Final length (cm)", "Final width (cm)")
+)
+
+# initial column measured a few days prior to week zero
+alllen <- read.csv(here::here('data/raw', 'length_data.csv'), stringsAsFactors = F) %>% 
+  clean_names() %>% 
+  select(week, trt = treatment, jar, id = individual_id, species, everything()) %>% 
+  group_by(species) %>% 
+  filter(!is.na(initial_length)) %>% 
+  mutate(
+    median = median(initial_length, na.rm = T), 
+    sizecl = case_when(
+      initial_length < median ~ 'small', 
+      initial_length >= median ~ 'large'
+    ),
+    delt_length = final_length - initial_length, 
+    delt_width = final_width - initial_width
+  ) %>% 
+  gather('var', 'val', -week, -trt, -jar, -id, -species, -sizecl, -median) %>% 
+  filter(week != 6) %>% 
+  filter(!(week %in% 0 & var %in% c('delt_length', 'delt_width'))) %>% 
+  filter(!var %in% c('initial_length', 'initial_width')) %>% 
+  mutate(
+    var = factor(var, levels = rspslen$shrtlab, labels = rspslen$lngslab), 
+    trt = factor(trt, levels = trts$shrtlab, labels = trts$shrtlab), 
+    jar = factor(jar)
+  )
+
+save(alllen, file = here('data', 'alllen.RData'), compress = 'xz')
+
+# fit separate mixed models by species, week, jar as random effect
+bymodslen <- alllen %>%
+  group_by(week, species, sizecl, var) %>% 
+  nest %>% 
+  mutate(
+    mixmod = pmap(list(var, data), function(var, data){
+      
+      tomod <- data %>%
+        mutate(
+          trt = fct_drop(trt),
+          jar = fct_drop(jar)
+        )
+      
+      # no replicate jars by treatment for whole weight
+      if(var == 'Whole weight (g)')
+        out <- glm(val ~ trt, data = tomod)
+      
+      if(var != 'Whole weight (g)')
+        out <- lmerTest::lmer(val ~ trt + (1|jar), data = tomod)
+      
+      return(out)
+      
+    }),
+    anomod = map(mixmod, function(x){
+      
+      # if(inherits(x, 'glm'))
+      #   out <- summary(x)$coefficients
+      # if(inherits(x, 'lmerModLmerTest'))
+      out <- anova(x)
+      
+      return(out)
+      
+    }), 
+    # summod = map(mixmod, analyze),
+    plomod = pmap(list(week, species, var, mixmod), function(week, species, var, mixmod){
+      
+      # estimates
+      mnsval <- get_means(mixmod, 'trt')
+      cnsval <- get_contrasts(mixmod, 'trt') %>% 
+        mutate(
+          sig = ifelse(p < 0.05, 'sig', 'notsig'),
+          sig = factor(sig,levels = c('notsig', 'sig'), labels = c(' not significant', 'significant'))
+        )
+      
+      # sample size
+      if(inherits(mixmod, 'glm'))
+        n <- mixmod$model %>% nrow
+      if(inherits(mixmod, 'lmerModLmerTest'))
+        n <- mixmod@frame %>% nrow
+      
+      # labels
+      subttl <- paste0(var, ', week ', week, ', ', species, ' oyster')
+      captns <- paste0('Significance is where CI does not include zero, alpha = 0.05, total n = ', n)
+      
+      # mean esimate plots
+      p1 <- ggplot(mnsval, aes(x = trt, y = Mean)) + 
+        geom_point(size = 3) + 
+        geom_errorbar(aes(ymin = CI_lower, ymax = CI_higher), colour = 'black', size = 1) + 
+        labs(x = NULL, y = 'Estimated means (+/- 95% CI)', title = 'Treatment estimates', subtitle = subttl) + 
+        theme_ipsum() + 
+        theme(
+          panel.grid.major.y = element_blank(),
+          panel.grid.minor.y = element_blank()
+        ) + 
+        coord_flip()
+      
+      # contrast plots
+      p2 <- ggplot(cnsval, aes(x = Contrast, y = Difference, colour = sig)) + 
+        geom_point(aes(colour = sig), size = 3) + 
+        geom_errorbar(aes(ymin = CI_lower, ymax = CI_higher, colour = sig), size = 1) + 
+        labs(x = NULL, y = 'Estimated differences (+/- 95% CI)', title = 'Treatment differences', subtitle = subttl,
+             caption = captns) +
+        theme_ipsum() + 
+        theme(
+          legend.title = element_blank(), 
+          legend.position = 'bottom', 
+          panel.grid.major.y = element_blank(),
+          panel.grid.minor.y = element_blank()
+        ) +
+        geom_hline(yintercept = 0, linetype = 'dotted', size = 1) + 
+        scale_colour_manual(drop = F, values = c('black', 'tomato1')) + 
+        coord_flip()
+      
+      p1 + p2 + plot_layout(ncol  = 2)   
+      
+      
+    })
+  )
+
+save(bymodslen, file = here::here('data', 'bymodslen.RData'), compress = 'xz')
+
+# treatment colors
+cls <- RColorBrewer::brewer.pal(6, 'BrBG')
+cls <- c(cls[1:3], cls[c(6, 4)])
+
+# fit separate mixed models by species, week, jar as random effect
+wtmodslen <- alllen %>%
+  group_by(species, sizecl, var) %>% 
+  nest %>% 
+  mutate(
+    mixmod = pmap(list(var, data), function(var, data){
+      
+      tomod <- data %>% 
+        mutate(
+          week = factor(week), 
+          jar = fct_drop(jar), 
+          trt = fct_drop(trt)
+        )
+      
+      # no replicate jars by treatment for whole weight
+      if(var == 'Whole weight (g)')
+        out <- glm(val ~ trt * week, data = tomod)
+      
+      if(var != 'Whole weight (g)')
+        out <- lmerTest::lmer(val ~ trt * week + (1|jar), data = tomod)
+      
+      return(out)
+      
+    }),
+    mixmodnoint = pmap(list(var, data), function(var, data){
+      
+      tomod <- data %>% 
+        mutate(
+          week = factor(week), 
+          jar = fct_drop(jar), 
+          trt = fct_drop(trt)
+        )
+      
+      # no replicate jars by treatment for whole weight
+      if(var == 'Whole weight (g)')
+        out <- glm(val ~ trt + week, data = tomod)
+      
+      if(var != 'Whole weight (g)')
+        out <- lmerTest::lmer(val ~ trt + week + (1|jar), data = tomod)
+      
+      return(out)
+      
+    }),
+    anomod = map(mixmod, function(x){
+      
+      # if(inherits(x, 'glm'))
+      #   out <- summary(x)$coefficients
+      # if(inherits(x, 'lmerModLmerTest'))
+      out <- anova(x)
+      
+      return(out)
+      
+    }), 
+    anomodnoint = map(mixmodnoint, function(x){
+      
+      # if(inherits(x, 'glm'))
+      #   out <- summary(x)$coefficients
+      # if(inherits(x, 'lmerModLmerTest'))
+      out <- anova(x)
+      
+      return(out)
+      
+    }), 
+    # summod = map(mixmod, analyze),
+    plomod = pmap(list(species, var, mixmod), function(species, var, mixmod){
+      
+      # estimates
+      mnsval <- get_means(mixmod, 'trt * week')
+      
+      # sample size
+      if(inherits(mixmod, 'glm'))
+        n <- mixmod$model %>% nrow
+      if(inherits(mixmod, 'lmerModLmerTest'))
+        n <- mixmod@frame %>% nrow
+      
+      # labels
+      subttl <- paste0(var, ', ', species, ' oyster')
+      captns <- paste0('Significance is where CI does not include zero, alpha = 0.05, total n = ', n)
+      
+      # mean esimate plots
+      p1 <- ggplot(mnsval, aes(x = week, y = Mean, group = trt, colour = trt, fill = trt)) + 
+        geom_errorbar(aes(ymin = CI_lower, ymax = CI_higher, colour = trt), width = 0, position = position_dodge(0.3), size = 1) + 
+        geom_line(position = position_dodge(0.3)) + 
+        geom_point(size = 3, position = position_dodge(0.3), pch = 21, colour = 'black') + 
+        labs(x = 'Exposure week', y = 'Estimated means (+/- 95% CI)', title = 'Treatment estimates', subtitle = subttl, caption = captns) + 
+        scale_colour_manual(values = cls) + 
+        scale_fill_manual(values = cls) + 
+        theme_ipsum() + 
+        theme(
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(), 
+          legend.title = element_blank()
+        )
+      
+      p1
+      
+    })
+  )
+
+save(wtmodslen, file = here::here('data', 'wtmodslen.RData'), compress = 'xz')
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # README
 
-Materials for the anlaysis of PS field data and lab experiments at SCCWRP
+Materials for the analysis of PS field data and lab experiments at SCCWRP
+
+Main lab results here: <https://fawda123.github.io/oyster_exposure/>
+
+Subset length analyses by size class here: <https://fawda123.github.io/oyster_exposure/length.html>
