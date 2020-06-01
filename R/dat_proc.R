74193c620ec5e48385dfa21544ab86f6efa516b8
fawda123 <mbafs2012@gmail.com>
2020-05-31 20:17:00 -0400

    models by week for individual growth differences

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -6,6 +6,7 @@ library(lmerTest)
 library(psycho)
 library(hrbrthemes)
 library(patchwork)
+library(modelbased)
 
 # consistent treatment terminology names
 trts <- tibble(
@@ -558,4 +559,179 @@ wtmodslen <- alllen %>%
   )
 
 save(wtmodslen, file = here::here('data', 'wtmodslen.RData'), compress = 'xz')
+
+# models by week tracking individual differences --------------------------
+
+trts <- tibble(
+  shrtlab = c("7.7C", "7.7A0.2", "7.7A0.5", "8.0C", "8.0A0.2"),
+  lngslab = c( "7.7 Constant", "7.7 Fluctuating 0.2A", "7.7 Fluctuating 0.5A", "8.0 Constant", "8.0 Fluctuating 0.2A")
+)
+
+# consistent response variable terminology names
+rsps <- tibble(
+  shrtlab = c('length', 'width', 'weight'),
+  lngslab = c("Final length (cm)", "Final width (cm)", "Whole weight (g)")
+)
+
+lendat <- read_excel(here::here('data/raw/Length_kelp_4_3_2020.xlsx')) %>% 
+  clean_names() %>% 
+  select(week, trt = treatment, jar, id = individual_id, species, final_length = length_cm_final, 
+         initial_length = length_cm_initial, final_width = width_cm_final, initial_width = width_cm_initial) %>% 
+  mutate(
+    delt_length = final_length - initial_length, 
+    init_length = 0,
+    delt_width = final_width - initial_width, 
+    init_width = 0
+  ) %>% 
+  filter(week != 0) %>% 
+  select(week, trt, jar, id, species, delt_length, init_length, delt_width, init_width) %>% 
+  gather('var', 'val', delt_length, init_length, delt_width, init_width) %>% 
+  separate(var, c('period', 'var')) %>% 
+  group_by(week, trt, jar, id, species, period, var) %>% 
+  summarise(val = mean(val, na.rm = T)) %>% 
+  ungroup %>% 
+  filter(!week == 0) %>% 
+  mutate(
+    week = case_when(
+      period == 'init' ~ 0, 
+      T ~ week
+    )
+  ) %>% 
+  filter(week != 0)
+
+wtdat <- read.csv(here::here('data/raw/Weight_with dead but not multiple_Kelp_4_3.csv'), stringsAsFactors = F) %>% 
+  clean_names() %>% 
+  # filter(week != 0) %>% 
+  filter(species != '') %>% 
+  mutate(
+    species = gsub('\\s*$', '', species),
+    whole_organism_weight = case_when(
+      is.na(whole_organism_weight) &  !is.na(shell_weight) & !is.na(tissue_weight) ~ shell_weight + tissue_weight, 
+      T ~ whole_organism_weight
+    )
+  ) %>% 
+  filter(!is.na(initial_wetweight) & !is.na(whole_organism_weight)) %>% 
+  select(week, trt = treatment, jar = i_jar, id = individual_id, species, initial_weight = initial_wetweight, 
+         final_weight = whole_organism_weight) %>% 
+  mutate(
+    delt_weight = final_weight - initial_weight, 
+    init_weight = 0,
+  ) %>% 
+  filter(week != 0) %>% 
+  select(week, trt, jar, id, species, delt_weight, init_weight) %>% 
+  gather('var', 'val', delt_weight, init_weight) %>% 
+  separate(var, c('period', 'var')) %>% 
+  group_by(week, trt, jar, id, species, period, var) %>% 
+  summarise(val = mean(val, na.rm = T)) %>% 
+  ungroup %>% 
+  filter(!week == 0) %>% 
+  mutate(
+    week = case_when(
+      period == 'init' ~ 0L, 
+      T ~ week
+    )
+  ) %>% 
+  filter(week != 0)
+
+tomod <- bind_rows(wtdat, lendat) %>% 
+  select(-period) %>% 
+  mutate(
+    trt = factor(trt, levels = trts$shrtlab), 
+    var = factor(var, levels = rsps$shrtlab, rsps$lngslab), 
+    jar = factor(jar)
+  )
+
+# fit separate mixed models by species, week, jar as random effect
+indbymods <- tomod %>%
+  group_by(week, species, var) %>% 
+  nest %>% 
+  mutate(
+    mixmod = pmap(list(var, data), function(var, data){
+      
+      tomod <- data %>%
+        mutate(
+          trt = fct_drop(trt),
+          jar = fct_drop(jar)
+        )
+      
+      # no replicate jars by treatment for whole weight
+      if(var == 'Whole weight (g)')
+        out <- glm(val ~ trt, data = tomod)
+      
+      if(var != 'Whole weight (g)')
+        out <- lmerTest::lmer(val ~ trt + (1|jar), data = tomod)
+      
+      return(out)
+      
+    }),
+    anomod = map(mixmod, function(x){
+      
+      # if(inherits(x, 'glm'))
+      #   out <- summary(x)$coefficients
+      # if(inherits(x, 'lmerModLmerTest'))
+      out <- anova(x)
+      
+      return(out)
+      
+    }),
+    # summod = map(mixmod, analyze),
+    plomod = pmap(list(week, species, var, mixmod), function(week, species, var, mixmod){
+      
+      # estimates
+      mnsval <- estimate_means(mixmod, 'trt') %>% 
+        data.frame
+      cnsval <- estimate_contrasts(mixmod, 'trt') %>%
+        mutate(
+          sig = ifelse(p < 0.05, 'sig', 'notsig'),
+          sig = factor(sig,levels = c('notsig', 'sig'), labels = c(' not significant', 'significant'))
+        ) %>% 
+        unite('Contrast', Level1, Level2, sep = '-')
+      
+      # sample size
+      if(inherits(mixmod, 'glm'))
+        n <- mixmod$model %>% nrow
+      if(inherits(mixmod, 'lmerModLmerTest'))
+        n <- mixmod@frame %>% nrow
+      
+      # labels
+      subttl <- paste0(var, ', week ', week, ', ', species, ' oyster')
+      captns <- paste0('Significance is where CI does not include zero, alpha = 0.05, total n = ', n)
+      
+      # mean esimate plots
+      p1 <- ggplot(mnsval, aes(x = trt, y = Mean)) +
+        geom_point(size = 3) +
+        geom_errorbar(aes(ymin = CI_low, ymax = CI_high), colour = 'black', size = 1) +
+        labs(x = NULL, y = 'Estimated means (+/- 95% CI)', title = 'Treatment estimates', subtitle = subttl) +
+        theme_ipsum() +
+        theme(
+          panel.grid.major.y = element_blank(),
+          panel.grid.minor.y = element_blank()
+        ) +
+        coord_flip()
+
+      # contrast plots
+      p2 <- ggplot(cnsval, aes(x = Contrast, y = Difference, colour = sig)) +
+        geom_point(aes(colour = sig), size = 3) +
+        geom_errorbar(aes(ymin = CI_low, ymax = CI_high, colour = sig), size = 1) +
+        labs(x = NULL, y = 'Estimated differences (+/- 95% CI)', title = 'Treatment differences', subtitle = subttl,
+             caption = captns) +
+        theme_ipsum() +
+        theme(
+          legend.title = element_blank(),
+          legend.position = 'bottom',
+          panel.grid.major.y = element_blank(),
+          panel.grid.minor.y = element_blank()
+        ) +
+        geom_hline(yintercept = 0, linetype = 'dotted', size = 1) +
+        scale_colour_manual(drop = F, values = c('black', 'tomato1')) +
+        coord_flip()
+      
+      p1 + p2 + plot_layout(ncol  = 2)
+      
+      
+    })
+  )
+
+save(indbymods, file = here('data/indbymod.RData'), compress = 'xz')
+
 
